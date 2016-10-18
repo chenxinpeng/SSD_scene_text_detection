5fc84d8dfed1d0c874a5f755e3a8cda693b9f60a
chenxinpeng <2015202130029@whu.edu.cn>
2016-10-18 22:42:13 +0800

    Delete VGG_scenetext_SSD_300x300.sh

--- VGG_scenetext_SSD_300x300.sh
+++
@@ -1,5 +0,0 @@
-cd /home/chenxp/caffe
-./build/tools/caffe train \
---solver="models/VGGNet/scenetext/SSD_300x300/solver.prototxt" \
---weights="models/VGGNet/VGG_ILSVRC_16_layers_fc_reduced.caffemodel" \
---gpu 0 | tee jobs/VGGNet/scenetext/SSD_300x300/VGG_scenetext_SSD_300x300.log
