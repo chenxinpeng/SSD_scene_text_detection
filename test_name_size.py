a34d8fe78042570bc847cd7b6d07206efd6d6ca2
chenxinpeng <2015202130029@whu.edu.cn>
2016-10-18 21:50:46 +0800

    Create test_name_size.py
    create the labelmap_voc.prototxt

---
+++ test_name_size.py
@@ -0,0 +1,17 @@
+#! /usr/bin/python
+
+import os, sys
+import glob
+from PIL import Image
+
+img_dir = "/home/chenxp/data/VOCdevkit/scenetext/JPEGImages"
+
+img_lists = glob.glob(img_dir + '/*.jpg')
+
+test_name_size = open('/home/chenxp/caffe/data/scenetext/test_name_size.txt', 'w')
+
+for item in img_lists:
+    img = Image.open(item)
+    width, height = img.size
+    temp1, temp2 = os.path.splitext(os.path.basename(item))
+    test_name_size.write(temp1 + ' ' + str(height) + ' ' + str(width) + '\n')
